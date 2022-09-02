e75be2e4b8f412b241fc1084a796b3f93c27abb0
Johnny Chen <Johnnychen384@gmail.com>
2022-09-02 04:02:05 -0400

    Updates

---
+++ precache-manifest.f7466663c50b58e8d55d1af56ab6c331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b9a06135182140c8ca6",
+    "url": "/React-Pick-A-Pic/static/css/main.5061709d.chunk.css"
+  },
+  {
+    "revision": "6b9a06135182140c8ca6",
+    "url": "/React-Pick-A-Pic/static/js/main.6b9a0613.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/React-Pick-A-Pic/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "8de0ce365a4fe6126f50",
+    "url": "/React-Pick-A-Pic/static/js/2.8de0ce36.chunk.js"
+  },
+  {
+    "revision": "3fbd1b622725dc3e0557",
+    "url": "/React-Pick-A-Pic/static/js/runtime~main.3fbd1b62.js"
+  },
+  {
+    "revision": "3036861aa3d301900ad8ac2af8b8b374",
+    "url": "/React-Pick-A-Pic/index.html"
+  }
+];
